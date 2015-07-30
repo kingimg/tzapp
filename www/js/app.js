058f24c7d5d88c712ef641073029f8a174d4337f
kingimg <kingimg@qq.com>
2015-07-30 14:57:16 +0800

    first

---
+++ www/js/app.js
@@ -0,0 +1,104 @@
+﻿// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleLightContent();
+        }
+
+    });
+})
+.filter('brDateFilter', function () {
+    return function (dateSTR) {
+        if (dateSTR) { dateSTR = dateSTR.replace("/Date(", "").replace(")/", ""); var now = new Date(parseInt(dateSTR)); var year = now.getFullYear(); var month = now.getMonth() + 1; var date = now.getDate(); var hour = now.getHours(); var minute = now.getMinutes(); var second = now.getSeconds(); return year + "-" + month + "-" + date; }        
+    }
+})
+.filter('delHtmlTag', function () {
+    return function (str) {
+        return str.replace(/<[^>]+>/g, "");
+    }
+})
+.filter('deviceStatusTran', function () {
+        return function (val) {
+            switch (val) {
+                case 0: return "闲置"; break;
+                case 1: return "已安装"; break;
+                case 2: return "已检测"; break;
+                case 3: return "使用中"; break;
+                case 4: return "报废"; break;
+                default: return val; break;
+            }
+        }
+    })
+.filter('machineIconTran', function () {
+    return function (val) {
+        switch (val) {
+            case 1: return "icon-construction15"; break;
+            case 2: return "icon-elevator3"; break;
+            case 3: return "icon-crane30"; break;
+            case 4: return "icon-excavator3"; break;
+        }
+    }
+})
+.config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider, $httpProvider) {
+    $httpProvider.defaults.headers.common['apikey'] = window.localStorage["apikey"];
+    //$httpProvider.interceptors.push('httpInterceptor');
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $ionicConfigProvider.tabs.position('bottom');
+    //if (!ionic.Platform.isIOS()) {
+    //    $ionicConfigProvider.scrolling.jsScrolling(false);
+    //}
+    $stateProvider
+    .state('menus', { url: "/menus", abstract: true, templateUrl: "templates/menus.html" })
+    .state('login', { url: "/login", templateUrl: "templates/login.html", controller: 'ctrl-login' })
+    .state('menus.home', { views: { 'menus-home': { templateUrl: 'templates/home.html', controller: 'ctrl-home' } }, url: '/home' })
+
+    .state('menus.monitor-main', { views: { 'menus-monitor': { templateUrl: 'templates/monitor/main.html', controller: 'ctrl-monitor-main' } }, url: '/monitor/main' })
+    .state('menus.monitor-device', { views: { 'menus-monitor': { templateUrl: 'templates/monitor/devices.html', controller: 'ctrl-monitor-devices' } }, url: '/monitor/devices/:monitorType/:monitorStatus' })
+    .state('menus.monitor-device-detail', { views: { 'menus-monitor': { templateUrl: 'templates/monitor/device-detail.html', controller: 'ctrl-monitor-devicedetail' } }, url: '/monitor/device/:deviceId' })
+    .state('menus.monitor-device-detail-info', { views: { 'menus-monitor': { templateUrl: 'templates/monitor/device-detail-info.html', controller: 'ctrl-monitor-devicedetail-info' } }, url: '/monitor/device/:deviceId/info' })
+
+    //.state('menus.monitor-elevators', { views: { 'menus-monitor': { templateUrl: 'templates/monitor/elevators.html', controller: 'ctrl-monitor-elevators' } }, url: '/monitor/elevators' })
+    //.state('menus.monitor-elevator-detail', { views: { 'menus-monitor': { templateUrl: 'templates/monitor/elevator-detail.html', controller: 'ctrl-monitor-elevatordetail' } }, url: '/monitor/elevators/:deviceId' })
+    //.state('menus.monitor-elevator-detail-info', { views: { 'menus-monitor': { templateUrl: 'templates/monitor/elevator-detail-info.html', controller: 'ctrl-monitor-elevatordetail-info' } }, url: '/monitor/elevators/:deviceId/info' })
+
+    .state('menus.monitor-warnings', { views: { 'menus-monitor': { templateUrl: 'templates/monitor/warnings.html', controller: 'ctrl-monitor-warnings' } }, url: '/monitor/warnings' })
+
+    .state('menus.monitor-machinerys', { views: { 'menus-monitor': { templateUrl: 'templates/monitor/machinerys.html', controller: 'ctrl-monitor-machinerys' } }, url: '/monitor/machinerys/:machinerysType' })
+    .state('menus.monitor-machinery-detail', { views: { 'menus-monitor': { templateUrl: 'templates/monitor/machinery-detail.html', controller: 'ctrl-monitor-machinerydetail' } }, url: '/monitor/machinerydetail/:baId' })
+
+    .state('menus.check-projects', { views: { 'menus-check': { templateUrl: 'templates/check/projects.html', controller: 'ctrl-check-projects' } }, url: '/check/projects' })
+    .state('menus.check-checks', { views: { 'menus-check': { templateUrl: 'templates/check/checks.html', controller: 'ctrl-check-checks' } }, url: '/check/projectchecks/:proId' })
+    .state('menus.check-checkdetail', { views: { 'menus-check': { templateUrl: 'templates/check/check-detail.html', controller: 'ctrl-check-checkdetail' } }, url: '/check/checkdetail/:operType/:checkId' })
+    .state('menus.check-checkadd', { views: { 'menus-check': { templateUrl: 'templates/check/check-detail.html', controller: 'ctrl-check-checkdetail' } }, url: '/check/checkadd/:operType/:proId' })
+
+    .state('menus.board-projects', { views: { 'menus-board': { templateUrl: 'templates/board/projects.html', controller: 'ctrl-board-projects' } }, url: '/board/projects' })
+    .state('menus.board-project-detail', { views: { 'menus-board': { templateUrl: 'templates/board/project-detail.html', controller: 'ctrl-board-projectdetail' } }, url: '/board/projects/:proId' })
+
+    .state('menus.account-main', { views: { 'menus-account': { templateUrl: 'templates/account/main.html', controller: 'ctrl-account-main' } }, url: '/account/main' })
+    .state('menus.account-userinfo', { views: { 'menus-account': { templateUrl: 'templates/account/userinfo.html', controller: 'ctrl-account-userinfo' } }, url: '/account/userinfo' })
+    .state('menus.account-zone', { views: { 'menus-account': { templateUrl: 'templates/account/zone.html', controller: 'ctrl-account-zone' } }, url: '/account/zone' })
+
+    .state("menus.common-company-detail", { views: { 'menus-monitor': { templateUrl: 'templates/common/company-detail.html', controller: 'ctrl-common-companydetail' } }, url: '/common/companys/:companyId' })
+    .state("menus.common-project-detail", { views: { 'menus-monitor': { templateUrl: 'templates/common/project-detail.html', controller: 'ctrl-common-projectdetail' } }, url: '/common/projects/:proId' })
+
+    ;  // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/login');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,391 @@
+﻿//angular.module('starter.controllers', [])
+
+
+angular.module('starter.controllers', []).directive('dateFormat', ['$filter', function ($filter) {
+    var dateFilter = $filter('date');  
+    return {  
+        require: 'ngModel',  
+        link: function(scope, elm, attrs, ctrl) {    
+            function formatter(value) {
+                return dateFilter(value, 'yyyy-MM-dd'); //format 
+            }    
+            function parser() {  
+                return ctrl.$modelValue;  
+            }  
+            ctrl.$formatters.push(formatter);  
+            ctrl.$parsers.unshift(parser);  
+        }  
+    };  
+}])
+
+.constant('ApiEndpoint', {
+    url: 'http://tzapp.safe110.net:8085'  
+//url: 'http://192.168.1.214:8085'  
+})
+
+
+.controller('ctrl-login', function ($scope, $http, $state, $ionicHistory, ApiEndpoint) {
+    $ionicHistory.nextViewOptions({ disableBack: true });
+    $scope.dologin = function () {
+        if (window.localStorage["apikey"] != undefined) {
+            //$state.go('menus.home');
+            $state.go('menus.monitor-main');
+        }
+    };
+    $scope.login = function (user) {
+        $http.get(ApiEndpoint.url + '/user/login/?loginName=' + user.username + '&passWord=' + user.password + '&r=' + Math.random()).success(function (data) {
+            if (data.success) {
+                window.localStorage['apikey'] = data.obj;
+                $http.defaults.headers.common['apikey'] = window.localStorage["apikey"];
+                //$state.go('menus.home');
+                $state.go('menus.monitor-main');
+            } else {
+                alert(data.msg);
+            }
+        }).error(function (response) {
+            if (errCb && typeof errCb === 'function') {
+                errCb(response);
+            }
+        });
+    };
+    $scope.dologin();
+})
+.controller('ctrl-home', function ($scope) {
+
+})
+.controller('ctrl-monitor-main', function ($scope, $http, $state, ApiEndpoint) {
+    $scope.homeData = {};
+    $http.post(ApiEndpoint.url + '/monitor/getDeviceCount?r=' + Math.random()).success(function (data) {
+        if (data.success) {
+            $scope.homeData.TD = data.rows.TD;//塔式起重机
+            $scope.homeData.SJJ = data.rows.SJJ;//施工升降机
+            $scope.homeData.WL = data.rows.WL;//物料提升机
+            $scope.homeData.QT = data.rows.QT;//其它设备
+            $scope.homeData.Total = data.rows.Total;//已备案设备总数
+
+            $scope.homeData.TotalRegisted = data.rows.TotalRegisted;//受监控的总量
+            $scope.homeData.TCRegisted = data.rows.TCRegisted;//登记塔吊
+            $scope.homeData.TCOnline = data.rows.TCOnline;//在线塔吊
+            $scope.homeData.TCOffline = data.rows.TCOffline;//离线塔吊
+            $scope.homeData.TCAlarm = data.rows.TCAlarm == undefined ? 0 : data.rows.TCAlarm;//违章塔吊
+            $scope.homeData.ERegisted = data.rows.ERegisted;//登记人货梯
+            $scope.homeData.EOnline = data.rows.EOnline;//在线人货梯
+            $scope.homeData.EOffline = data.rows.EOffline;//离线人货梯
+            $scope.homeData.EAlarm = data.rows.EAlarm == undefined ? 0 : data.rows.EAlarm;//违章人货梯
+        } else {
+            alert(data.msg);
+            $state.go('login');
+        }
+    });
+})
+.controller('ctrl-monitor-devices', function ($scope, $stateParams, $http, $state, ApiEndpoint) {
+    var url = "";
+    $scope.devices = [];
+    switch ($stateParams.monitorStatus) {
+        case '1': url = '/monitor/getRegisterList?monitorType=' + $stateParams.monitorType + '&r=' + Math.random(); break;
+        case '2': url = '/monitor/getOnlineDevice?monitorType=' + $stateParams.monitorType + '&r=' + Math.random(); break;
+        case '3': url = '/monitor/getTodayAlarmList?monitorType=' + $stateParams.monitorType + '&r=' + Math.random(); break;
+    }
+    $http.post(ApiEndpoint.url + url).success(function (data) {
+        if (data.success) {
+            $scope.devices = $scope.devices.concat(data.rows);
+            $scope.$broadcast("scroll.infiniteScrollComplete");           
+        } else {
+            alert('登录超时，请重新登录');
+            $scope.login();
+            return;
+        }
+    });
+})
+    .controller('ctrl-monitor-devicedetail', function ($scope) {
+
+    })
+    .controller('ctrl-monitor-devicedetail-info', function ($scope) {
+
+    })
+    //.controller('ctrl-monitor-elevators', function ($scope) {
+
+    //})
+    //.controller('ctrl-monitor-elevatordetail', function ($scope) {
+
+    //})
+    //.controller('ctrl-monitor-elevatordetail-info', function ($scope) {
+
+    //})
+    .controller('ctrl-monitor-machinerys', function ($scope, $stateParams, $http, $state, ApiEndpoint) {
+        $scope.machinerys = [];
+        $scope.page = 1;
+        $scope.page_count = 1;
+        $scope.load_over = true;
+        $scope.loadMore = function () {
+            $http.post(ApiEndpoint.url + '/filing/getFilingMachinerys/?page=' + $scope.page + '&machinerysType=' + $stateParams.machinerysType + '&r=' + Math.random()).success(function (data) {
+                if (data.success) {
+                    $scope.page_count = data.total;
+                    $scope.machinerys = $scope.machinerys.concat(data.rows);
+                    $scope.page++;
+                    $scope.$broadcast("scroll.infiniteScrollComplete");
+                    if ($scope.page > $scope.page_count) {
+                        $scope.load_over = false;
+                        return;
+                    }
+                } else {
+                    alert('登录超时，请重新登录');
+                    $scope.login();
+                    return;
+                }
+            })
+        };
+        $scope.doRefresh = function () {
+            $scope.$broadcast('scroll.refreshComplete');
+        };
+    })
+    .controller('ctrl-monitor-machinerydetail', function ($scope, $stateParams, $http, $state, ApiEndpoint) {
+        $scope.machinerydetail = {};
+        $http.post(ApiEndpoint.url + '/filing/getFilingMachineryDetail?baId=' + $stateParams.baId + '&r=' + Math.random()).success(function (data) {
+            if (data.success) {
+                $scope.machinerydetail.BeiAnICP = data.rows.BeiAnICP;
+                $scope.machinerydetail.SpecificationModel = data.rows.SpecificationModel;
+                $scope.machinerydetail.IsOutland = data.rows.IsOutland == 0 ? "本地机械" : "外地机械";
+                $scope.machinerydetail.ApplyMan = data.rows.ApplyMan;
+                $scope.machinerydetail.MadeDate = data.rows.MadeDate;
+                $scope.machinerydetail.UseEndDate = data.rows.UseEndDate;
+                $scope.machinerydetail.FactoryNO = data.rows.FactoryNO;
+                $scope.machinerydetail.FactoryName = data.rows.FactoryName;
+                $scope.machinerydetail.PropertyCompanyName = data.rows.PropertyCompanyName;
+            } else {
+                alert(data.msg);
+                $state.go('login');
+            }
+        });
+    })
+    .controller('ctrl-monitor-warnings', function ($scope) {
+
+    })
+.controller('ctrl-check-projects', function ($scope, $http, $state, ApiEndpoint) {
+    $scope.projects = [];
+    $scope.page = 1;
+    $scope.page_count = 1;
+    $scope.load_over = true;
+    $scope.loadMore = function () {
+        $http.post(ApiEndpoint.url + '/zaSys/checkProList/?page=' + $scope.page + '&r=' + Math.random()).success(function (data) {
+            if (data.success) {
+                $scope.page_count = data.total;
+                $scope.projects = $scope.projects.concat(data.rows);
+                $scope.page++;
+                $scope.$broadcast("scroll.infiniteScrollComplete");
+                if ($scope.page > $scope.page_count) {
+                    $scope.load_over = false;
+                    return;
+                }
+            } else {
+                alert('登录超时，请重新登录');
+                $scope.login();
+                return;
+            }
+        })
+    };
+    //$scope.moreDataCanBeLoaded = function () { return CheckProjects.hasmore; };
+    $scope.doRefresh = function () {
+        $scope.$broadcast('scroll.refreshComplete');
+    };
+})
+
+.controller('ctrl-check-checks', function ($scope, $stateParams, $http, $state, ApiEndpoint) {
+    $scope.checks = [];
+    $scope.page = 1;
+    $scope.page_count = 1;
+    $scope.load_over = true;
+    $scope.loadMore = function () {
+        $http.post(ApiEndpoint.url + '/zaSys/checkList/?page=' + $scope.page + '&proid=' + $stateParams.proId + '&r=' + Math.random()).success(function (data) {
+            if (data.success) {
+                $scope.page_count = data.total;
+                $scope.checks = $scope.checks.concat(data.rows);
+                $scope.page++;
+                $scope.$broadcast("scroll.infiniteScrollComplete");
+                if ($scope.page > $scope.page_count) {
+                    $scope.load_over = false;
+                    return;
+                }
+            } else {
+                alert('登录超时，请重新登录');
+                $scope.login();
+                return;
+            }
+        })
+    };
+    $scope.doRefresh = function () {
+        $scope.$broadcast('scroll.refreshComplete');
+    };
+    $scope.addnew = function () {
+        $state.go("menus.check-checkadd", { operType:'add', proId: $stateParams.proId });
+    };
+})
+.controller('ctrl-check-checkdetail', function ($scope, $stateParams, $http ,$ionicActionSheet, ApiEndpoint) {
+    $scope.checkdetail = {};
+    $scope.photos = [];
+    if ($stateParams.operType=='edit'){        
+        $http.post(ApiEndpoint.url + '/zaSys/checkDetail/?checkId=' + $stateParams.checkId + '&r=' + Math.random()).success(function (data) {
+            if (data.success) {               
+                $scope.checkdetail.ProName = data.rows[0].ProName;
+                $scope.checkdetail.CheckTypeName = data.rows[0].CheckTypeName;
+                $scope.checkdetail.CheckTime = data.rows[0].CheckTime;
+                $scope.checkdetail.CheckUser = data.rows[0].CheckUser;
+                $scope.checkdetail.Content1 = data.rows[0].Content1;
+                $scope.photos = $scope.photos.concat(data.obj);
+            } else {
+                alert('登录超时，请重新登录');
+                $scope.login();
+                return;
+            }
+        })
+    }
+    
+    $scope.showActionsheet = function () {
+        $ionicActionSheet.show({
+            titleText: '上传现场照片',
+            cancelText: '取消',
+            buttons: [{ text: '拍照' }, { text: '从相册中选取' }],
+            cancel: function () { },
+            buttonClicked: function (index) {
+                switch (index) {
+                    case 1: takePhoto(); break;
+                    case 0:
+                    default: makePhoto(); break;
+                }
+                return true;
+            }
+        });                    
+    };
+    $scope.savecheck = function () {
+        if ($stateParams.operType == 'edit'){
+            $http.post(ApiEndpoint.url + '/zaSys/editCheck/?checkId=' + $stateParams.checkId + '&checkTypeName=' + $scope.checkdetail.CheckTypeName + '&checkTime=' + $scope.checkdetail.CheckTime + '&checkUser=' + $scope.checkdetail.CheckUser + '&content1=' + $scope.checkdetail.Content1 + '&r=' + Math.random()).success(function (data) {
+                if (data.success) {
+                    
+                } else {
+                    alert('登录超时，请重新登录');
+                    $scope.login();
+                    return;
+                }
+            })
+        }
+    };
+    function onPhotoDone(imageURI) {       
+        //$scope.photos.push(photo);
+        uploadPhoto(imageURI);
+    }
+    function onPhotoFail(message) {       
+        if (message.indexOf('cancelled') < 0) {
+            alert('出錯了：' + message);
+        }
+    }
+    function uploadPhoto(imageURI) {        
+        var done = function (r) {
+            var reg = eval('(' + r.response + ')');
+            var photo = { PicPath:reg.obj,PicPath_s: imageURI };
+            var temparr = [];
+            temparr.push(photo);
+            $scope.photos = $scope.photos.concat(temparr);
+            //ybb_user.profile.avatar = json.avatar_src;         
+        };
+
+        var fail = function (e) {
+            alert(e.code);          
+        };
+
+        var options = new FileUploadOptions();
+        options.fileKey = "fileAddPic";
+        options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
+        //如果是图片格式，就用image/jpeg，其他文件格式上官网查API
+        options.mimeType = "image/jpeg";          
+        options.chunkedMode = false;
+        var ft = new FileTransfer();               
+        ft.upload(imageURI, encodeURI(ApiEndpoint.url + "/common/upload/?r=" + Math.random()), done, fail, options);    
+    }
+    function makePhoto() {
+        navigator.camera.getPicture(onPhotoDone, onPhotoFail, {
+            quality: 100, targetWidth: 800, targetHeight: 800, destinationType: navigator.camera.DestinationType.FILE_URI
+        });
+    }
+    function takePhoto() {
+        navigator.camera.getPicture(onPhotoDone, onPhotoFail, {
+            quality: 100, targetWidth: 800, targetHeight: 800, destinationType: navigator.camera.DestinationType.FILE_URI
+        , sourceType: navigator.camera.PictureSourceType.SAVEDPHOTOALBUM
+        });
+    }
+})
+.controller('ctrl-board-projects', function ($scope, $http, $state, ApiEndpoint) {
+    $scope.projects = [];
+    $scope.page = 1;
+    $scope.page_count = 1;
+    $scope.load_over = true;
+    $scope.loadMore = function () {
+        $http.post(ApiEndpoint.url + '/zaSys/checkProList/?page=' + $scope.page + '&r=' + Math.random()).success(function (data) {
+            if (data.success) {
+                $scope.page_count = data.total;
+                $scope.projects = $scope.projects.concat(data.rows);
+                $scope.page++;
+                $scope.$broadcast("scroll.infiniteScrollComplete");
+                if ($scope.page > $scope.page_count) {
+                    $scope.load_over = false;
+                    return;
+                }
+            } else {
+                alert('登录超时，请重新登录');
+                $scope.login();
+                return;
+            }
+        })
+    };
+    $scope.doRefresh = function () {
+        $scope.$broadcast('scroll.refreshComplete');
+    };
+})
+
+.controller('ctrl-board-projectdetail', function ($scope, $stateParams, BoardProjects) {
+    $scope.shownGroup = 1;
+    $scope.toggleGroup = function (index) {
+        if ($scope.isGroupShown(index)) {
+            $scope.shownGroup = null;
+        } else {
+            $scope.shownGroup = index;
+        }
+    };
+    $scope.isGroupShown = function (index) {
+        return $scope.shownGroup === index;
+    };
+})
+
+.controller('ctrl-account-main', function ($scope, $state, $http, ApiEndpoint) {
+    $scope.exit = function () {
+        $http.get(ApiEndpoint.url + '/user/exit/?r=' + Math.random()).success(function (data) {
+            if (data.success) {
+                window.localStorage.removeItem('apikey'); $http.defaults.headers.common['apikey'] = "";
+                $state.go('login');
+            } else {
+                alert(data.msg);
+            }
+        }).error(function (response) {
+            if (errCb && typeof errCb === 'function') {
+                errCb(response);
+            }
+        });
+    };
+})
+.controller('ctrl-account-userinfo', function ($scope) {
+
+})
+.controller('ctrl-account-zone', function ($scope) {
+
+})
+
+.controller('ctrl-common-companydetail', function ($scope) {
+
+})
+.controller('ctrl-common-projectdetail', function ($scope) {
+
+})
+;
+
+
+
+
