3ea7b7627deb69afc569c493ece26f23a603eac1
kingimg <kingimg@qq.com>
2016-03-11 10:47:39 +0800

    更新

--- js/controllers.js
+++ js/controllers.js
@@ -19,10 +19,10 @@ angular.module('starter.controllers', [])
 //}])
 
 .constant('ApiEndpoint', {   
-   //url: 'http://192.168.3.63:8085'    
+   url: 'http://192.168.3.63:8085'    
    //url: 'http://tzapp.safe110.net:8085'
     //url: 'http://121.199.75.88:8085'
-     url:'http://183.129.189.108:8085'
+     //url:'http://183.129.189.108:8085'
 })
 
 
