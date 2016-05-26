f9aa516bb2b608315df8d6adfea64f4a365f45ca
kingimg <kingimg@qq.com>
2016-05-26 14:40:21 +0800

    测试

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -105,11 +105,10 @@ angular.module('starter.controllers', [])
     $scope.multitest = function ()
     {
         window.imagePicker.getPictures(
-            function (results,results2) {
+            function (results) {
                 for (var i = 0; i < results.length; i++) {
                     //console.log('Image URI: ' + results[i]);
-                    alert(results[i]);
-                    alert(results2[i]);
+                    alert(results[i])
                 }
             }, function (error) {
                 console.log('Error: ' + error);
